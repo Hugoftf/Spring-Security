73a9d1181b492ca3ae80f911856ad15c2481e6ca
Hugo de França Torres Ferreira <f3rreirahugo@hotmail.com>
2025-03-30 12:40:07 -0300

    Imagens iniciais do programa

--- target/generated-sources/annotations/com/github/Hugoftf/Spring/JPA/controller/mappers/AutorMapperImpl.java
+++ target/generated-sources/annotations/com/github/Hugoftf/Spring/JPA/controller/mappers/AutorMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-26T14:10:00-0300",
+    date = "2025-03-30T12:25:12-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
 )
 @Component
